24a37c616b44e22d586bc632ac2f6e65031bebd8
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-16 13:53:10 -0500

    Prompt written.

--- CPP-PRG-13-05-RetailItem-Class/main.cpp
+++ CPP-PRG-13-05-RetailItem-Class/main.cpp
@@ -5,11 +5,28 @@
 //  Created by Keith Smith on 11/16/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a class named RetailItem that holds data about an item in a retail store. The
+//  class should have the following member variables:
+//
+//  * description - a string that holds a brief descirption of the item
+//  * unitsOnHand - an int that holds the number of units currently in inventory
+//  * price - a double that holds the item's retail price
+//
+//  Write a constructor that accepts arguments for each member variable, appropriate mutator
+//  functions that store values in these member variables, and accessor functions that
+//  return the values in these member variables. Once you have written the class, write a
+//  separate program that creates three RetailItem objects and stores the following data in them:
+//
+//                          Description             Units On Hand               Price
+//  ----------------------------------------------------------------------------------------
+//  Item #1                 Jacket                  12                          59.95
+//  Item #2                 Designer Jeans          40                          34.95
+//  Item #3                 Shirt                   20                          24.95
+//
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
     return 0;
 }
